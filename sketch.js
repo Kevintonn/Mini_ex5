2d298b9435c1520fbd165f23e2277718b90e54ad
Kevintonn <natraps300@gmail.com>
2017-03-12 21:17:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -36,15 +36,15 @@ function draw() {
     
     fill (0);
     textSize(18);
-    text("And death shall have no dominion.", window.innerWidth-150, window.innerHeight);
-    text("Dead man naked they shall be one", window.innerWidth-150, window.innerHeight+30);
-    text("With the man in the wind and the west moon;", window.innerWidth-150, window.innerHeight+60);
-    text("When their bones are picked clean and the clean bones gone,", mouseX, mouseY+90);
-    text("They shall have stars at elbow and foot;", mouseX, mouseY+120);
-    text("Though they go mad they shall be sane,", mouseX, mouseY+150);
-    text("Though they sink through the sea they shall rise again;", mouseX, mouseY+180);
-    text("Though lovers be lost love shall not; ", mouseX, mouseY+210);
-    text("And death shall have no dominion.", mouseX, mouseY+240);
+    text("And death shall have no dominion.", window.innerWidth/2-150, 200);
+    text("Dead man naked they shall be one", window.innerWidth/2-150, 230);
+    text("With the man in the wind and the west moon;", window.innerWidth/2-150, 260);
+    text("When their bones are picked clean and the clean bones gone,", window.innerWidth/2-150, 290);
+    text("They shall have stars at elbow and foot;", window.innerWidth/2-150, 320);
+    text("Though they go mad they shall be sane,", window.innerWidth/2-150, 350);
+    text("Though they sink through the sea they shall rise again;", window.innerWidth/2-150, 380);
+    text("Though lovers be lost love shall not; ", window.innerWidth/2-150, 410);
+    text("And death shall have no dominion.", window.innerWidth/2-150, 440);
     
     
     
